efc2d8e2c6201f657c52f2fba6843d4f710ab2fb
unxpctdchrs <theunexpectedchris@gmail.com>
2024-03-09 14:29:47 +0700

    removed 'edge'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import Navbar from "@/components/Navbar"
 import DataList from "@/components/DataList"
 import Add from "@/components/Add"
 
-export const runtime = 'edge' // 'nodejs' (default) | 'edge'
-
 export default function Home() {
   return(
     <main>
--- src/lib/apiManager.ts
+++ src/lib/apiManager.ts
@@ -44,4 +44,4 @@ export async function updateData(data: DATA) {
       console.error('Error updating data:', error);
       throw error;
     }
-  }
+}
